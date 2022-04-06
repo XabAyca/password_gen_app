62cc413dccd2e7b79e76b3f623cb1bee42e261bc
Xabi <xabi.aycaguer@gmail.com>
2022-04-06 13:11:03 +0200

    create: controllers

--- lib/password_generator.ex
+++ lib/password_generator.ex
@@ -6,4 +6,10 @@ defmodule PasswordGenerator do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  @password_lengths [
+    Weak: Enum.map(6..15, & &1),
+    Strong: Enum.map(16..88, & &1),
+    Unbelievable: [100, 150]
+  ]
+  def password_lengths, do: @password_lengths
 end
--- lib/password_generator_web/controllers/page_controller.ex
+++ lib/password_generator_web/controllers/page_controller.ex
@@ -2,6 +2,21 @@ defmodule PasswordGeneratorWeb.PageController do
   use PasswordGeneratorWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    lengths = PasswordGenerator.password_lengths
+
+    conn
+      |> assign(:password_lengths, lengths)
+      |> assign(:password, "")
+      |> render("index.html")
+  end
+
+  def generate(conn, %{"password" => password_params}) do
+    lengths = PasswordGenerator.password_lengths
+    {:ok, password} = PassGenerator.generate(password_params)
+
+    conn
+      |> assign(:password_lengths, lengths)
+      |> assign(:password, password)
+      |> render("index.html")
   end
 end
